2937a5516c6de62aa37e010c951aabfab092750e
THancock810 <tommoocow@gmail.com>
2021-04-21 16:46:31 -0400

    Update CDC.js

--- client/src/views/CDC/CDC.js
+++ client/src/views/CDC/CDC.js
@@ -15,23 +15,24 @@ function CDC() {
             <header className="App-header"> 
                 <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer"></a> 
             </header>
-            
-            <h1>Information from the CDC </h1>
-
-        <div class="description">
-            <h2>To stay up to date on the most recent news from the CDC, please read below</h2>
-            <p> 
-                <a href="https://twitter.com/CDCgov">Please click here to stay up to date!</a>
-            </p>
+            <div class="container">
+                <div class="row justify-content-center">
+                    <div class="col d-flex justify-content-center">
+                        <h1>Information from the CDC </h1>
+                    </div>
+                </div>
+                <div class="row justify-content-center">
+                    <div class="col d-flex justify-content-center">
+                        <div class="description">
+                            <h2>To stay up to date on the most recent news from the CDC, please read below</h2>
+                            <p> <a href="https://twitter.com/CDCgov">Please click here to stay up to date!</a> </p>
             {/*<div><a class="twitter-timeline" data-width="300" data-height="500" href="https://twitter.com/CDCgov?ref_src=twsrc%5Etfw">Tweets by CDCgov</a> <script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script></div>
             */}
-            
-
-            <TwitterContainer></TwitterContainer>
-              
-
-        </div>
-
+                            <TwitterContainer></TwitterContainer>
+                        </div>
+                    </div>
+                </div>
+            </div>
     </div> 
     );
 }
